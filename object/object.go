83fb0e2f5161cfc4b1ddba6815d32c1aef613683
senicko <25861704+senicko@users.noreply.github.com>
2025-02-11 19:53:28 +0100

    Add support for builtin functions

--- object/object.go
+++ object/object.go
@@ -18,6 +18,7 @@ const (
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	FUNCTION_OBJ     = "FUNCTION"
 	ERROR_OBJ        = "ERROR"
+	BUILTIN_OBJ      = "BUILTIN"
 )
 
 type Object interface {
@@ -82,6 +83,15 @@ func (f *Function) Inspect() string {
 
 	return out.String()
 }
+
+type BuiltinFunction func(args ...Object) Object
+
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string  { return "builtin function" }
 
 type Error struct {
 	Message string
